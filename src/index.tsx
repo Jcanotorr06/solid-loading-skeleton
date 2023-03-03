0339d9b240da5e6178f77d1536eb45612999e5e3
Jcanotorr06 <jcano062001@gmail.com>
2023-03-02 22:34:14 -0500

    update: add exports from index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export type { SkeletonProps, SkeletonStyleProps, SkeletonThemeProps } from "./types"
 export { SkeletonTheme, useSkeletonTheme } from "./SkeletonTheme"
 export { SkeletonThemeContext } from "./SkeletonThemeContext"
-export { default } from "./Skeleton"
+export { default, Skeleton } from "./Skeleton"
